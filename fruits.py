f3894da88a78562f42beec3de5f62a9d3c2583c4
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-17 19:43:08 -0300

    Create fruits.py

---
+++ fruits.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+my_fruits = {"apple", "orange", "pineapple", "tomato"}
+friend_fruits = {"banana", "cherry", "cashew", "fig"}
+
+union_result = my_fruits | friend_fruits
+print("Union: ", union_result)
+
+intersection_result = my_fruits.intersection(friend_fruits)
+print(intersection_result)
+
+different_result = my_fruits - friend_fruits
+print(different_result)
